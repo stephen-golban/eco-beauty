23266593dee6a5aa34018c10efc2e5f3d448b380
Stephen Golban <golban.stephen@gmail.com>
2025-05-04 18:22:32 +0300

    Update dependencies in package.json and bun.lock; enable postinstall script for Prisma generation

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -228,7 +228,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -229,7 +229,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
